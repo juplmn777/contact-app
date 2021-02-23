737808e3376999cdb30dc66dca9cc0968cbd6dc5
juplmn777 <julien.plumyoen@gmail.com>
2021-02-23 12:05:08 +0100

    all 3 cards added and styled

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
 import Card from './components/Card';
-import contacts from './components/contacts'
+import contacts from './components/contacts';
 
 export default function App() {
   return (
     <div className="container pt-6 pb-6">
+      <h1 className="title is-1 has-text-centered">A Simple Contact App</h1>
       <div className="columns is-centered">
         <div className="column is-4">
-          <Card 
+          <Card
             name={contacts[0].name}
             img={contacts[0].imgURL}
             tel={contacts[0].phone}
             mail={contacts[0].email}
+          />
+        </div>
+        <div className="column is-4">
+          <Card
+            name={contacts[1].name}
+            img={contacts[1].imgURL}
+            tel={contacts[1].phone}
+            mail={contacts[1].email}
+          />
+        </div>
+        <div className="column is-4">
+          <Card
+            name={contacts[2].name}
+            img={contacts[2].imgURL}
+            tel={contacts[2].phone}
+            mail={contacts[2].email}
           />
         </div>
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,25 +4,27 @@ export default function Card(props) {
   return (
     <Fragment>
       <div className="card">
-        <div className="card-image">
-          <figure className="image is-4by3">
-            <img src={props.img} alt="contact"/>
+        <div className="card-image px-5 py-5">
+          <figure className="image is-256x256">
+            <img src={props.img} alt="contact" />
           </figure>
         </div>
         <div className="card-content">
           <div className="media">
             <div className="media-left">
               <figure className="image is-48x48">
-                <img src={props.img} alt="contact tmblr"/>
+                <img src={props.img} alt="contact tmblr" />
               </figure>
             </div>
             <div className="media-content">
-              <p className="title is-4">{props.name}</p>
-              <p className="subtitle is-6">{props.mail}</p>
+              <p className="title is-4 has-text-light">{props.name}</p>
+              <p className="subtitle is-6 has-text-light">{props.mail}</p>
             </div>
           </div>
-          <div className="content">
-            <p className="title is-4">Tel: {props.tel}</p>
+          <div className="content has-text-centered">
+            <p className="title is-4 has-text-light">
+              Tel: <span className="has-text-weight-light">{props.tel}</span>
+            </p>
           </div>
         </div>
       </div>
