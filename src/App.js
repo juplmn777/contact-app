a0dfd852bd2826e3715f5870d67ac759483a7557
juplmn777 <julien.plumyoen@gmail.com>
2021-02-23 17:59:28 +0100

    contacts array mapped

--- src/App.js
+++ src/App.js
@@ -3,36 +3,24 @@ import 'bulma/css/bulma.min.css';
 import Card from './components/Card';
 import contacts from './components/contacts';
 
+const createCard = (contact) => {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      mail={contact.email}
+    />
+  );
+};
+
 export default function App() {
   return (
     <div className="container pt-4">
       <h1 className="title is-1 has-text-centered">A Simple Contact App</h1>
       <div className="columns is-centered">
-        <div className="column is-4">
-          <Card
-            name={contacts[0].name}
-            img={contacts[0].imgURL}
-            tel={contacts[0].phone}
-            mail={contacts[0].email}
-          />
-        </div>
-        <div className="column is-4">
-          <Card
-            name={contacts[1].name}
-            img={contacts[1].imgURL}
-            tel={contacts[1].phone}
-            mail={contacts[1].email}
-          />
-        </div>
-        <div className="column is-4">
-          <Card
-            name={contacts[2].name}
-            img={contacts[2].imgURL}
-            tel={contacts[2].phone}
-            mail={contacts[2].email}
-          />
-        </div>
+        {contacts.map(createCard)}</div>
       </div>
-    </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,22 +7,24 @@ import TinyAvatar from './TinyAvatar';
 export default function Card(props) {
   return (
     <Fragment>
-      <div className="card">
-        <div className="card-image px-5 py-5">
-          <Avatar img={props.img} />
-        </div>
-        <div className="card-content">
-          <div className="media">
-            <div className="media-left">
-              <TinyAvatar img={props.img} />
-            </div>
-            <div className="media-content">
-              <ContactName contactName={props.name} />
-            </div>
+      <div className="column is-4">
+        <div className="card">
+          <div className="card-image px-5 py-5">
+            <Avatar img={props.img} />
           </div>
-          <div className="content has-text-left">
-            <CardDetail cardInfo={props.mail} />
-            <CardDetail cardInfo={props.tel} />
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+                <TinyAvatar img={props.img} />
+              </div>
+              <div className="media-content">
+                <ContactName contactName={props.name} />
+              </div>
+            </div>
+            <div className="content has-text-left">
+              <CardDetail cardInfo={props.mail} />
+              <CardDetail cardInfo={props.tel} />
+            </div>
           </div>
         </div>
       </div>
